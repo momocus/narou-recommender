8accd6fd1815a543b186308f5a981eaba7293701
momocus <momocus333@gmail.com>
2020-01-13 03:37:49 +0900

    svmによる機械学習

--- fav_novel_classifier.py
+++ fav_novel_classifier.py
@@ -6,16 +6,16 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score
 from sklearn.metrics import recall_score, f1_score, roc_curve, auc
+from abc import ABCMeta, abstractmethod
+from sklearn.svm import SVC
 
-# 参考：　https://qiita.com/0NE_shoT_/items/b702ab482466df6e5569
 
-
-class LogisticRegressionModel(object):
+class Model(metaclass=ABCMeta):
 
     def __init__(self):
         self.kyoshi = analyze_data.get_kyoshi()
 
-    def pick_data(self):
+    def load_data(self):
         # 目的変数
         # pointが1か2か(好きかめっちゃ好きか)はといったん区別しないでおく
         objective = self.kyoshi.point.apply(lambda x: x > 0)
@@ -27,26 +27,50 @@ class LogisticRegressionModel(object):
              "keyword", "general_firstup", "general_lastup", "time",
              "isbl", "isgl", "pc_or_k", "novelupdated_at", "updated_at",
              "istensei", "istenni", "iszankoku", "isr15",
-             "writer", "point", "ncode"], axis=1)
+             "writer", "point", "ncode"], axis=1).fillna(0)
 
         # 80%のデータを学習データに、20%を検証データにする
         self.exp_train, self.exp_test, self.obj_train, self.obj_test = \
             train_test_split(explanatory, objective,
                              test_size=0.2, random_state=0)
 
-    def make_model(self):
-        # モデルの作成（トレーニング）
-        lr = LogisticRegression()
-        lr.fit(self.exp_train, self.obj_train)  # モデルの重みを学習
-        with open('lr.pickle', mode='wb') as fp:
-            pickle.dump(lr, fp)
-
-    def load_model(self):
-        with open('lr.pickle', mode='rb') as fp:
+    def load(self):
+        with open(self.modelname, mode='rb') as fp:
             lr = pickle.load(fp)
         self.model = lr
 
-    def test_model(self):
+    def classfy(self):
+        target = analyze_data.get_target()
+        explanatory = target.drop(
+            ["title", "story", "userid", "gensaku",
+             "keyword", "general_firstup", "general_lastup", "time",
+             "isbl", "isgl", "pc_or_k", "novelupdated_at", "updated_at",
+             "istensei", "istenni", "iszankoku", "isr15",
+             "writer", ], axis=1).fillna(0)
+        return self.model.predict(explanatory)
+
+    @abstractmethod
+    def make(self):
+        pass
+
+    @abstractmethod
+    def test(self):
+        pass
+
+
+class LogisticRegressionModel(Model):
+
+    modelname = "./model/lrm.pickle"
+
+    def make(self):
+        # モデルの作成（トレーニング）
+        lr = LogisticRegression()
+        lr.fit(self.exp_train, self.obj_train)  # モデルの重みを学習
+        self.model = lr
+        with open(self.modelname, mode='wb') as fp:
+            pickle.dump(lr, fp)
+
+    def test(self):
 
         # モデルの性能評価
         obj_pred = self.model.predict(self.exp_test)
@@ -80,20 +104,37 @@ class LogisticRegressionModel(object):
         plt.ylabel("true positive rate")
         plt.show()
 
-    def classfy(self):
-        target = analyze_data.get_target()
-        explanatory = target.drop(
-            ["title", "story", "userid", "gensaku",
-             "keyword", "general_firstup", "general_lastup", "time",
-             "isbl", "isgl", "pc_or_k", "novelupdated_at", "updated_at",
-             "istensei", "istenni", "iszankoku", "isr15",
-             "writer", ], axis=1)
-        return self.model.predict(explanatory)
+
+class SVMModel(Model):
+
+    modelname = "./model/svm.pickle"
+
+    def make(self):
+        # モデルの作成（トレーニング）
+        model = SVC(kernel='linear', random_state=None)
+        model.fit(self.exp_train, self.obj_train)  # モデルの重みを学習
+        self.model = model
+        with open(self.modelname, mode='wb') as fp:
+            pickle.dump(model, fp)
+
+    def test(self):
+        obj_pred = self.model.predict(self.exp_test)
+        print("confusion matrix = \n", confusion_matrix(
+            y_true=self.obj_test, y_pred=obj_pred))
+        print("accuracy = ", accuracy_score(
+            y_true=self.obj_test, y_pred=obj_pred))
+        print("precision = ", precision_score(
+            y_true=self.obj_test, y_pred=obj_pred))
+        print("recall = ", recall_score(y_true=self.obj_test, y_pred=obj_pred))
+
+        print("f1 score = ", f1_score(y_true=self.obj_test, y_pred=obj_pred))
 
 
 def main():
-    lr = LogisticRegressionModel()
-    lr.pick_data()
+    lrm = LogisticRegressionModel()
+    lrm.load_data()
+    lrm.make()
+    lrm.test()
 
 
 if __name__ == "__main__":
